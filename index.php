4cd7b9025bbd96da1d9baa00c6eb841d441b446a
2wr <taolaolgc@gmail.com>
2023-03-18 22:05:01 +0700

    finish the login function

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ function log($text) {
 
 function login($email, $password) {
 	$url = "https://mbasic.facebook.com/";
+	$login_url = "https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100";
+
 	$cookie_file = "jar.txt";
 	$agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41";
 	
@@ -25,7 +27,7 @@ function login($email, $password) {
 		CURLOPT_COOKIEJAR => $cookie_file,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_USERAGENT => $agent,
-		CURLOPT_HEADER => true
+		CURLOPT_HTTPHEADER, ['Accept-Language: en']
 	]);
 
 	$facebook_res = curl_exec($ch);
@@ -41,8 +43,6 @@ function login($email, $password) {
 	$unrecognized_tries = $html->find("input[name=unrecognized_tries]")->value;
 	$bi_xrwh = $html->find("input[name=bi_xrwh]")->value;
 	
-	include "secret.php";
-
 	$form = [
 		"lsd" => $lsd,
 		"jazoest" => $jazoest,
@@ -51,10 +51,26 @@ function login($email, $password) {
 		"try_number" => $try_number,
 		"unrecognized_tries" => $unrecognized_tries,
 		"email" => $email,
-		"pass" => $pass,
+		"pass" => $password,
 		"bi_xrwh" => $bi_xrwh,
 		"login" => "Log in"
 	];
+
+	$ch = curl_init();
+	curl_setopt_array($ch, [
+		CURLOPT_URL => $login_url,
+		CURLOPT_CONNECTTIMEOUT => 30,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_COOKIEJAR => $cookie_file,
+		CURLOPT_COOKIEFILE => $cookie_file,
+		CURLOPT_POST => true,
+		CURLOPT_POSTFIELDS => http_build_query($form),
+		CURLOPT_HTTPHEADER, ['Accept-Language: en']
+	]);
+	curl_exec($ch);
+
+	return $ch;
 }
 
 switch ($method) {
@@ -62,6 +78,23 @@ switch ($method) {
 		if (!($_REQUEST["code"] == "kekw")) {
 			break;
 		}
+
+		if (!($_REQUEST["command"] == "login")) {
+			break;
+		}
+
+		$ch = login($email, $pass);
+		curl_setopt_array($ch, [
+			CURLOPT_URL => "https://mbasic.facebook.com/",
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_COOKIEFILE => "jar.txt",
+			CURLOPT_COOKIEJAR => "jar.txt"
+		]);
+		$res = curl_exec($ch);
+
+		echo $res;
+
 		break;
 }
 
